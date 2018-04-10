d467659385c409921fa0ab60debfad75d5cf4b94
Nvertigo <nvertigo67@gmail.com>
2018-04-10 16:03:22 +0200

    op3t: update build fingerprint to ob25.
    Change-Id: I0d936ab9647efd9c2eb88095d77a63269f164ca8

--- init/init_oneplus3.cpp
+++ init/init_oneplus3.cpp
@@ -123,7 +123,7 @@ void load_op3t(const char *model) {
     property_override("ro.build.product", "OnePlus3");
     property_override_dual("ro.product.device", "ro.vendor.product.device", "OnePlus3T");
     property_override("ro.build.description", "OnePlus3-user 8.0.0 OPR1.170623.032 31 release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "OnePlus/OnePlus3/OnePlus3T:8.0.0/OPR1.170623.032/02281230:user/release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "OnePlus/OnePlus3/OnePlus3T:8.0.0/OPR1.170623.032/04101010:user/release-keys");
     property_set("ro.power_profile.override", "power_profile_3t");
 }
 
